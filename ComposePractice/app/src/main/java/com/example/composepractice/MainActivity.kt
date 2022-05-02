88bf61be5f593952b579fd89d6281d44ddb5c2b8
kimjjunho <kimjjun1007@gmail.com>
2022-05-02 15:49:43 +0900

    Start::ComposePractice
    compose 기능 연습

---
+++ ComposePractice/app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.composepractice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composepractice.ui.theme.ComposePracticeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            setContent {
+                ComposePracticeTheme {
+                    Surface(color = MaterialTheme.colors.background) {
+                        Greeting("Android")
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun Greeting(name: String) {
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = ""
+            )
+            Column {
+                Text(text = "이름 : $name!")
+                Text(text = "특징 : 귀엽게 생김")
+            }
+        }
+    }
+
+    @Preview(showSystemUi = true, showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        ComposePracticeTheme {
+            Greeting("Android")
+        }
+    }
+}
