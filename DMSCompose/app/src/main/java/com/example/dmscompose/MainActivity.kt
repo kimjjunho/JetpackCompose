582b2afba34b0cd4a572c608a9103df335e97f0b
kimjjunho <kimjjun1007@gmail.com>
2022-05-02 15:35:30 +0900

    init::Image
    이미지 넣기

--- DMSCompose/app/src/main/java/com/example/dmscompose/MainActivity.kt
+++ DMSCompose/app/src/main/java/com/example/dmscompose/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.dmscompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.dmscompose.ui.theme.DMSComposeTheme
 
@@ -28,8 +31,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Row{
-        Text(text = name)
-        Text(text = "Hello $name!")
+        Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "")
+        Column{
+            Text(text = "이름 : $name!")
+            Text(text = "특징 : 귀엽게 생김")
+        }
     }
 }
 
--- README.md
+++ README.md
@@ -37,3 +37,31 @@ fun Greeting(name: String) {
 //요소를 수평으로 정렬
 ```
 
+### Box
+
+```kotlin
+@Composable
+fun Greeting(name: String) {
+    Box{
+        Text(text = "Hello $name!")
+        Text(text = "Hello $name!")
+    }
+}
+//요소를 쌓을 수 있음
+```
+
+### Image
+
+```kotlin
+@Composable
+fun Greeting(name: String) {
+    Row{
+        Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "") //이미지를 추가하는 코드, contentDescription : 이미지를 대체할 설명
+        Column{
+            Text(text = "이름 : $name!")
+            Text(text = "특징 : 귀엽게 생김")
+        }
+    }
+}
+```
+
