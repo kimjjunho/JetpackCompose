d088b74e9e8966409a3c9fd3db9ec7c1f8ad4074
kimjjunho <kimjjun1007@gmail.com>
2022-05-03 23:38:53 +0900

    fix::UI Setting

--- DMSCompose/app/src/main/java/com/example/dmscompose/ui/theme/Type.kt
+++ DMSCompose/app/src/main/java/com/example/dmscompose/ui/theme/Type.kt
@@ -3,8 +3,10 @@ package com.example.dmscompose.ui.theme
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.dmscompose.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
