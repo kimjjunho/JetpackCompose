30ac62f589d0b4972a2a239692d68c040bcf4274
kimjjunho <kimjjun1007@gmail.com>
2022-05-21 19:17:59 +0900

    fix::MoveMintBar
    아래항목에 포함

--- DMSCompose/app/src/main/java/com/example/dmscompose/mypage/CenterMintBar.kt
+++ DMSCompose/app/src/main/java/com/example/dmscompose/mypage/CenterMintBar.kt
@@ -1,2 +1,41 @@
 package com.example.dmscompose.mypage
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.dmscompose.ui.theme.Mint
+import com.example.dmscompose.ui.theme.font
+
+@Composable
+fun centerMintBar(text:String){
+    Box(
+        modifier = Modifier
+            .height(30.dp)
+            .fillMaxWidth()
+            .background(
+                color = Mint,
+                shape = RoundedCornerShape(15.dp)
+            )
+    ) {
+        Text(
+            text = text,
+            color = Color.White,
+            fontFamily = font,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 11.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .fillMaxHeight()
+                .wrapContentHeight(Alignment.CenterVertically)
+        )
+    }
+}
