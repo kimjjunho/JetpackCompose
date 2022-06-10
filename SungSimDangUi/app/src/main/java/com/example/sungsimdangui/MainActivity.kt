0b1f69ddd5571cfd170e993f3e212aea7134dde6
kimjjunho <kimjjun1007@gmail.com>
2022-06-10 11:33:29 +0900

    Start::SungSimDangUi

---
+++ SungSimDangUi/app/src/main/java/com/example/sungsimdangui/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.sungsimdangui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.sungsimdangui.ui.theme.SungSimDangUiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SungSimDangUiTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showSystemUi = true,showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SungSimDangUiTheme {
+        Greeting("Android")
+    }
+}
